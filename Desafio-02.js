a93b5b18b1d7836c00f38c31decff1511b871abf
Rodrigo Ceballos <frahumada.dev@gmail.com>
2024-04-23 23:58:23 -0300

    entrega desafio 2

---
+++ Desafio-02.js
@@ -0,0 +1,257 @@
+const { log } = require("console");
+const fs = require("fs");
+
+class ProductManager {
+  constructor(path) {
+    this.products = path;
+  }
+
+  async addProduct(title, description, price, thumbnail, code, stock) {
+    const product = {
+      id: this.#getMaxId() + 1,
+      title,
+      description,
+      price,
+      thumbnail,
+      code,
+      stock,
+    };
+
+    try {
+      const products = await this.getProducts();
+      if (!this.#getPrductByCode(product.code)) {
+        products.push(product);
+        await fs.promises.writeFile(this.path, JSON.stringify(products));
+        console.log("PRODUCTO AGREGADO (" + product.code + ")");
+      } else {
+        console.log(
+          "Error! This code (" + product.code + ") is already exists"
+        );
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async deletePrductByCode(code) {
+    try {
+      const products = await this.getProducts();
+      let productToDelete = this.#getPrductByCode(code);
+      let indexToDelete = products.indexOf(productToDelete);
+      if (productToDelete) {
+        products.splice(indexToDelete, 1);
+        await fs.promises.writeFile(this.path, JSON.stringify(products));
+        console.log("PRODUCTO ELIMINADO (" + productToDelete.code + ")");
+      } else {
+        console.log("Error! This code (" + code + ") is not added");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async getProducts() {
+    try {
+      if (fs.existsSync(this.path)) {
+        const products = await fs.promises.readFile(this.path, "utf8");
+        return JSON.parse(products);
+      } else {
+        return [];
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async getPrductById(productId) {
+    try {
+      const products = await this.getProducts();
+      if (products.find((product) => product.id === productId)) {
+        return products.find((product) => product.id === productId);
+      } else {
+        console.log("Error! product not found");
+        return false;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async #getPrductByCode(productCode) {
+    try {
+      const products = await this.getProducts();
+      if (products.find((product) => product.code === productCode)) {
+        return products.find((product) => product.code === productCode);
+      } else {
+        return false;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async #getMaxId() {
+    try {
+      const products = await this.getProducts();
+      let maxId = 0;
+      products.map((product) => {
+        if (product.id > maxId) {
+          maxId = product.id;
+        }
+        return maxId;
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async updateProduct(
+    productId,
+    title = "",
+    description = "",
+    price = false,
+    thumbnail = "",
+    code = false,
+    stock = false
+  ) {
+    try {
+      const products = await this.getProducts();
+      const productToUpdate = this.getPrductById(productId);
+      if (title) {
+        productToUpdate.title = title;
+      } else if (description) {
+        productToUpdate.description = description;
+      } else if (price) {
+        productToUpdate.price = price;
+      } else if (thumbnail) {
+        productToUpdate.thumbnail = thumbnail;
+      } else if (code) {
+        productToUpdate.code = code;
+      } else if (stock) {
+        productToUpdate.stock = stock;
+      } else {
+        console.log("NO HAY CAMPOS PARA ACTUALIZAR");
+      }
+      products.push(productToUpdate);
+      await fs.promises.writeFile(this.path, JSON.stringify(products));
+      console.log("PRODUCTO MODIFICADO (" + productToUpdate.code + ")");
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+
+function showTitle(title) {
+  const large = title.length;
+  const maxCharacter = 70;
+
+  if (large > maxCharacter) {
+    console.log("* " + title + " *");
+  } else {
+    const tab = Math.round((maxCharacter - large) / 2);
+    const equalLine = "=".repeat(maxCharacter);
+    const spaceLine = " ".repeat(tab);
+
+    console.log(equalLine);
+    console.log(spaceLine + title + spaceLine);
+    console.log(equalLine);
+  }
+}
+
+//create a new productManager
+const productManager = new ProductManager("./products.json");
+
+//call "getProducts" to get an empty array
+showTitle("get an empty array");
+emptyList = productManager.getProducts();
+console.log(emptyList);
+
+//add new product
+showTitle("Add new product");
+productManager.addProduct(
+  "producto prueba",
+  "Este es un producto prueba",
+  200,
+  "Sin imagen",
+  "abc123",
+  25
+);
+productManager.addProduct(
+  "Tablet",
+  "Articulo de tecnologia",
+  150000,
+  "htttp://www.google.com",
+  123,
+  11
+);
+productManager.addProduct(
+  "Celular",
+  "Articulo de tecnologia",
+  100000,
+  "htttp://www.google.com",
+  124,
+  5
+);
+productManager.addProduct(
+  "SmartWatch",
+  "Articulo de tecnologia",
+  88000,
+  "htttp://www.google.com",
+  125,
+  15
+);
+productManager.addProduct(
+  "Cartucho",
+  "Articulo de impresion",
+  12000,
+  "htttp://www.google.com",
+  126,
+  10
+);
+
+//listing products
+showTitle("Show products");
+const lista = productManager.getProducts();
+console.log(lista);
+
+//get a product by ID
+showTitle("Get an existing product by ID");
+const traerProductoPorId = productManager.getPrductById(2);
+console.log(traerProductoPorId);
+
+showTitle("Get an unexisting product by ID");
+const traerSegundoProductoPorId = productManager.getPrductById(9999);
+console.log(traerSegundoProductoPorId);
+
+//Update a product
+showTitle("Update a product");
+productManager.updateProduct(
+  3,
+  (desciption = "SE CAMBIO LA DESCRIPCION DEL PRODUCTO CON ID = 3")
+);
+
+//Delete an unexisting product and an existing product
+showTitle("Delete an unexisting product");
+productManager.deletePrductByCode(111);
+
+showTitle("Delete an existing product");
+productManager.deletePrductByCode(125);
+
+//Add an existing product
+showTitle("Add an existing product");
+productManager.addProduct(
+  "Cartucho",
+  "Articulo de impresion",
+  12000,
+  "htttp://www.google.com",
+  126,
+  10
+);
+
+//show the product list
+showTitle("Show products");
+console.log(lista);
+
+//show the product list
+showTitle("Show products");
+console.log(lista);
