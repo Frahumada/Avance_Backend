98233037ad17c1652d5e7ba8b44fbad79ffd93c9
Rodrigo Ceballos <frahumada.dev@gmail.com>
2024-04-30 19:47:31 -0300

    correcciones y cambios principalmente en la funcion updateProduct

--- Desafio-02.js
+++ Desafio-02.js
@@ -3,12 +3,15 @@ const fs = require("fs");
 
 class ProductManager {
   constructor(path) {
-    this.products = path;
+    this.path = path;
+    this.products = [];
   }
 
-  addProduct = async(title, description, price, thumbnail, code, stock) => {
+
+
+  addProduct = async (title, description, price, thumbnail, code, stock) => {
     const product = {
-      id: this.#getMaxId() + 1,
+      id: (await this.#getMaxId()) + 1,
       title,
       description,
       price,
@@ -22,11 +25,15 @@ class ProductManager {
       const products = await this.getProducts();
 
       //Valido que no este repetido el producto
-      if (!this.#getPrductByCode(product.code)) {
+      const nuevo = await this.#getPrductByCode(product.code);
+      if (!nuevo) {
         products.push(product);
 
         //guardo productos actualizado
-        await fs.promises.writeFile(this.path, JSON.stringify(products));
+        await fs.promises.writeFile(
+          this.path,
+          JSON.stringify(products, null, 4)
+        );
         console.log("PRODUCTO AGREGADO (" + product.code + ")");
       } else {
         console.log(
@@ -44,21 +51,27 @@ class ProductManager {
     } catch (error) {
       console.log(error);
     }
-  }
+  };
 
-  deletePrductByCode = async(code) => {
-
-      const products = await this.getProducts();
-      let productToDelete = this.#getPrductByCode(code);
-      let indexToDelete = products.indexOf(productToDelete);
-      if (productToDelete) {
-        products.splice(indexToDelete, 1);
-        await fs.promises.writeFile(this.path, JSON.stringify(products));
-        console.log("PRODUCTO ELIMINADO (" + productToDelete.code + ")");
-      } else {
-        console.log("Error! This code (" + code + ") is not added");
+  deletePrductByCode = async (code) => {
+    const products = await this.getProducts();
+    let productToDelete = await this.#getPrductByCode(code);
+    console.log(productToDelete);
+    let indexToDelete = -1;
+    for (let i = 0; i < products.length; i++) {
+      if (products[i].code === code) {
+        indexToDelete = i;
+        break
       }
     }
+    if (productToDelete) {
+      products.splice(indexToDelete, 1);
+      await fs.promises.writeFile(this.path, JSON.stringify(products));
+      console.log("PRODUCTO ELIMINADO (" + productToDelete.code + ")");
+    } else {
+      console.log("Error! This code (" + code + ") is not added");
+    }
+  };
 
   getProducts = async () => {
     try {
@@ -83,58 +96,61 @@ class ProductManager {
     }
   };
 
-  #getPrductByCode = async(productCode) => {
-      const products = await this.getProducts();
-      if (products.find((product) => product.code === productCode)) {
-        return products.find((product) => product.code === productCode);
-      } else {
-        return false;
-      }
-  }
+  #getPrductByCode = async (productCode) => {
+    const products = await this.getProducts();
+    if (products.find((product) => product.code === productCode)) {
+      return products.find((product) => product.code === productCode);
+    } else {
+      return false;
+    }
+  };
 
-   #getMaxId = async() => {
-      const products = await this.getProducts();
-      let maxId = 0;
-      products.map((product) => {
-        if (product.id > maxId) {
-          maxId = product.id;
-        }
-        return maxId;
-      });
-  }
-
-  updateProduct = async (
-    productId,
-    title = "",
-    description = "",
-    price = false,
-    thumbnail = "",
-    code = false,
-    stock = false
-  ) => {
-      const products = await this.getProducts();
-      const productToUpdate = this.getPrductById(productId);
-      if (title) {
-        productToUpdate.title = title;
-      } else if (description) {
-        productToUpdate.description = description;
-      } else if (price) {
-        productToUpdate.price = price;
-      } else if (thumbnail) {
-        productToUpdate.thumbnail = thumbnail;
-      } else if (code) {
-        productToUpdate.code = code;
-      } else if (stock) {
-        productToUpdate.stock = stock;
-      } else {
-        console.log("NO HAY CAMPOS PARA ACTUALIZAR");
+  #getMaxId = async () => {
+    const products = await this.getProducts();
+    let maxId = 0;
+    products.map((product) => {
+      if (product.id > maxId) {
+        maxId = product.id;
       }
-      products.push(productToUpdate);
+    });
+    return maxId;
+  };
+
+  updateProduct = async (productId, field, content) => {
+    const products = await this.getProducts();
+    const productToUpdate = products.find((product) => product.id === productId);
+    let flag = false;
+    console.log(field.toLowerCase());
+    console.log(content.toLowerCase());
+    if (field.toLowerCase() == "title") {
+      productToUpdate.title = content;
+      flag = true;
+    } else if (field.toLowerCase() == "description") {
+      productToUpdate.description = content;
+      flag = true;
+    } else if (field.toLowerCase() == "price") {
+      productToUpdate.price = content;
+      flag = true;
+    } else if (field.toLowerCase() == "thumbnail") {
+      productToUpdate.thumbnail = content;
+      flag = true;
+    } else if (field.toLowerCase() == "code") {
+      productToUpdate.code = content;
+      flag = true;
+    } else if (field.toLowerCase() == "stock") {
+      productToUpdate.stock = content;
+      flag = true;
+    } else {
+      console.log("NO HAY CAMPOS PARA ACTUALIZAR");
+    }
+    if (flag) {
+      // await products.push(productToUpdate);
+      // await this.deletePrductByCode(productToUpdate.code);
       await fs.promises.writeFile(this.path, JSON.stringify(products));
       console.log("PRODUCTO MODIFICADO (" + productToUpdate.code + ")");
     }
   };
-
+}
 
 function showTitle(title) {
   const large = title.length;
@@ -156,110 +172,102 @@ function showTitle(title) {
 //create a new productManager
 const productManager = new ProductManager("./products.json");
 
+const test = async () => {
+  //create a empty archive of the product
+  await fs.promises.writeFile("./products.json", "[]");
 
-const test = async() => {
+  //call "getProducts" to get an empty array
+  showTitle("get an empty array");
+  emptyList = await productManager.getProducts();
+  console.log(emptyList);
 
-//create a empty archive of the product
-await fs.promises.writeFile("./products.json","[]");
+  //add new product
+  showTitle("Add new product");
 
-//call "getProducts" to get an empty array
-showTitle("get an empty array");
-emptyList = await productManager.getProducts();
-console.log(emptyList);
+  await productManager.addProduct(
+    "producto prueba",
+    "Este es un producto prueba",
+    200,
+    "Sin imagen",
+    "abc123",
+    25
+  );
+  await productManager.addProduct(
+    "Tablet",
+    "Articulo de tecnologia",
+    150000,
+    "htttp://www.google.com",
+    123,
+    11
+  );
+  await productManager.addProduct(
+    "Celular",
+    "Articulo de tecnologia",
+    100000,
+    "htttp://www.google.com",
+    124,
+    5
+  );
+  await productManager.addProduct(
+    "SmartWatch",
+    "Articulo de tecnologia",
+    88000,
+    "htttp://www.google.com",
+    125,
+    15
+  );
+  await productManager.addProduct(
+    "Cartucho",
+    "Articulo de impresion",
+    12000,
+    "htttp://www.google.com",
+    126,
+    10
+  );
 
-//add new product
-showTitle("Add new product");
+  //listing products
+  showTitle("Show products");
+  let lista = await productManager.getProducts();
+  console.log(lista);
 
-//listing products
+  //get a product by ID
+  showTitle("Get an existing product by ID");
+  const traerProductoPorId = await productManager.getPrductById(2);
+  console.log(traerProductoPorId);
 
-await productManager.addProduct(
-  "producto prueba",
-  "Este es un producto prueba",
-  200,
-  "Sin imagen",
-  "abc123",
-  25
-);
-await productManager.addProduct(
-  "Tablet",
-  "Articulo de tecnologia",
-  150000,
-  "htttp://www.google.com",
-  123,
-  11
-);
-await productManager.addProduct(
-  "Celular",
-  "Articulo de tecnologia",
-  100000,
-  "htttp://www.google.com",
-  124,
-  5
-);
-await productManager.addProduct(
-  "SmartWatch",
-  "Articulo de tecnologia",
-  88000,
-  "htttp://www.google.com",
-  125,
-  15
-);
-await productManager.addProduct(
-  "Cartucho",
-  "Articulo de impresion",
-  12000,
-  "htttp://www.google.com",
-  126,
-  10
-);
+  showTitle("Get an unexisting product by ID");
+  const traerSegundoProductoPorId = await productManager.getPrductById(9999);
+  console.log(traerSegundoProductoPorId);
 
-//listing products
-showTitle("Show products");
-const lista = await productManager.getProducts();
-console.log(lista);
+  //Update a product
+  showTitle("Update a product");
+  await productManager.updateProduct(3,"description", "SE CAMBIO LA DESCRIPCION DEL PRODUCTO CON ID = 3");
+  showTitle("Show products");
+  lista = await productManager.getProducts();
+  console.log(lista);
 
-//get a product by ID
-showTitle("Get an existing product by ID");
-const traerProductoPorId = await productManager.getPrductById(2);
-console.log(traerProductoPorId);
+  //Delete an unexisting product and an existing product
+  showTitle("Delete an unexisting product");
+  await productManager.deletePrductByCode(111);
 
-showTitle("Get an unexisting product by ID");
-const traerSegundoProductoPorId = await productManager.getPrductById(9999);
-console.log(traerSegundoProductoPorId);
+  showTitle("Delete an existing product");
+  await productManager.deletePrductByCode(125);
 
-//Update a product
-showTitle("Update a product");
-// await productManager.updateProduct(
-//   3,
-//   (desciption = "SE CAMBIO LA DESCRIPCION DEL PRODUCTO CON ID = 3")
-// );
+  //Add an existing product
+  showTitle("Add an existing product");
+  await productManager.addProduct(
+    "Cartucho",
+    "Articulo de impresion",
+    12000,
+    "htttp://www.google.com",
+    126,
+    10
+  );
 
-//Delete an unexisting product and an existing product
-showTitle("Delete an unexisting product");
-await productManager.deletePrductByCode(111);
-
-showTitle("Delete an existing product");
-await productManager.deletePrductByCode(125);
-
-//Add an existing product
-showTitle("Add an existing product");
-await productManager.addProduct(
-  "Cartucho",
-  "Articulo de impresion",
-  12000,
-  "htttp://www.google.com",
-  126,
-  10
-);
-
-//show the product list
-showTitle("Show products");
-console.log(lista);
-
-//show the product list
-showTitle("Show products");
-console.log(lista);
-
-}
+  //show the product list
+  showTitle("Show products");
+  lista = await productManager.getProducts();
+  console.log(lista);
+};
 
 test();
